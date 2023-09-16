c65e77160e26c5a18cb70e8449fad75da77520e2
Hanaf <hanaf@Hanafs-MacBook-Pro.local>
2023-09-16 09:23:47 +0300

    3rd commit

--- lib/widget/new_expense.dart
+++ lib/widget/new_expense.dart
@@ -1,4 +1,5 @@
 
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class NewExpense extends StatefulWidget {
@@ -13,6 +14,21 @@ const NewExpense({super.key});
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+      context: context, 
+      initialDate: now, 
+      firstDate: firstDate, 
+      lastDate: now);
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
 
   @override
   void dispose() {
@@ -34,17 +50,58 @@ class _NewExpenseState extends State<NewExpense> {
             label: Text("Title"),
           ),
         ),
-        TextField(
+        Row(children: [
+          Expanded(child: TextField(
           controller: _amountController,
-          maxLength: 10,
           keyboardType: TextInputType.number,
           decoration: const InputDecoration(
             prefixText: '\$ ',
             label: Text("Amount"),
           ),
         ),
-        Row(children: [
-          TextButton(onPressed: () {}, 
+        ),
+        const SizedBox(width: 16),
+        Expanded(child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+          Text(_selectedDate == null 
+            ? "No Date Selected" 
+            : formatter.format(_selectedDate!),
+            ),
+          IconButton(onPressed: _presentDatePicker, 
+          icon: const Icon(Icons.calendar_month),
+          ),
+          ],
+        ),
+      ),
+    ],
+  ),
+      const SizedBox(height: 16,),
+        Row(
+          children: [
+            DropdownButton(
+              value: _selectedCategory,
+              items: Category.values.map(
+              (category) => DropdownMenuItem(
+                value: category,
+                child: Text(
+                  category.name.toUpperCase(),
+                ),
+                ),
+              ).toList(),
+            onChanged: (value) {
+              if (value == null) {
+                return;
+              }
+              setState(() {
+                _selectedCategory = value;
+              });
+            }),
+            const Spacer(),
+          TextButton(onPressed: () {
+            Navigator.pop(context);
+          }, 
           child: const Text("Cancel"),
           ),
           ElevatedButton(onPressed: () {
